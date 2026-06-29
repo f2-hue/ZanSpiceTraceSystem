c11d09295029dea95eaef69a2478f4b86c1ebd87
f2-hue <fahadmohamediidrisa@gmail.com>
2026-06-29 11:51:13 +0300

    initial setup

---
+++ ZanSpiceTraceSystem/src/main/java/Znz/ZanSpiceTraceSystem/ZanSpiceTraceSystemApplication.java
@@ -0,0 +1,13 @@
+package Znz.ZanSpiceTraceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZanSpiceTraceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZanSpiceTraceSystemApplication.class, args);
+	}
+
+}
---
+++ ZanSpiceTraceSystem/src/test/java/Znz/ZanSpiceTraceSystem/ZanSpiceTraceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Znz.ZanSpiceTraceSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZanSpiceTraceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
